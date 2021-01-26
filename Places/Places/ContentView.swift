0cb2b7e37d4cc74d0e277deb299085cff897ffc5
bobgodwinx <bobgodwinx@gmail.com>
2021-01-26 14:41:09 +0100

    Initial introduction of places a SwiftUI MVVM example

---
+++ Places/Places/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Places
+//
+//  Created by Bob Godwin Obi on 26.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Places/Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Bob Godwin Obi on 26.01.21.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
