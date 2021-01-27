e57bb1a92bf901a1bb361092863f7e02f4478ab8
bobgodwinx <bobgodwinx@gmail.com>
2021-01-27 21:46:52 +0100

    Intial introduction of Endpoint enum

---
+++ Places/Shared/Services/Endpoint.swift
@@ -0,0 +1,28 @@
+//
+//  Endpoint.swift
+//  Places
+//
+//  Created by Bob Godwin Obi on 27.01.21.
+//
+
+import Foundation
+
+enum Endpoint:String, CustomStringConvertible, RawRepresentable {
+    static let baseURLString = "https://bobgodwinx.github.io/trekr/Assets"
+    case location
+    case tips
+    
+    var description: String {
+        switch self {
+        case .location: return "location"
+        case .tips: return "tips"
+        }
+    }
+    
+    var rawValue: String {
+        switch self {
+        case .location: return "\(Self.baseURLString)/Json/locations.json"
+        case .tips: return "\(Self.baseURLString)/Json/tips.json"
+        }
+    }
+}
