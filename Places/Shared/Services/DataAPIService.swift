5680ab0bbf8258dd432e752359039ab7a102dc19
bobgodwinx <bobgodwinx@gmail.com>
2021-01-28 17:12:08 +0100

    Initial introduction of DataAPIService

---
+++ Places/Shared/Services/DataAPIService.swift
@@ -0,0 +1,40 @@
+//
+//  DataAPIService.swift
+//  Places
+//
+//  Created by Bob Godwin Obi on 28.01.21.
+//
+
+import Foundation
+import Combine
+
+protocol DataServiceUseCase {
+    func fetchLocations() -> AnyPublisher<[Location], APIError>
+    func fetchTips() -> AnyPublisher<[Tip], APIError>
+}
+
+struct DataAPIService {
+    private func request<T: Decodable>(with query: URLRequest) -> AnyPublisher<T, APIError> {
+        ///Add implementation 
+    }
+}
+
+extension DataAPIService: DataServiceUseCase {
+    func fetchTips() -> AnyPublisher<[Tip], APIError> {
+        request(with: URLRequest(endpoint: .tips))
+    }
+    
+    func fetchLocations() -> AnyPublisher<[Location], APIError> {
+        request(with: URLRequest(endpoint: .location))
+    }
+}
+
+extension URLRequest {
+    init(endpoint: Endpoint, method: HTTPMethod = .get) {
+        let str = endpoint.rawValue
+        let url = URL(string: str).unwrap()
+        self.init(url: url)
+        self.httpMethod = method.rawValue
+    }
+}
+
