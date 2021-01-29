789a839d405e264204971cef3a7b2ed820b96440
bobgodwinx <bobgodwinx@gmail.com>
2021-01-29 13:04:18 +0100

    Intial introduction of ImageGalleryViewModel

---
+++ Places/Shared/ViewModel/ImageGalleryViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  ImageGalleryViewModel.swift
+//  Places
+//
+//  Created by Bob Godwin Obi on 29.01.21.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+class ImageGalleryViewModel: ObservableObject {
+    /// Bindable properties
+    @Published  private(set) var locationImages = [LocationImage]()
+
+    private var bag = Set<AnyCancellable>()
+    private var imageService: ImageAPIServiceUseCase
+    private var dataService: DataServiceUseCase
+    
+    init(_ imageService: ImageAPIServiceUseCase, _ dataService: DataServiceUseCase)  {
+        self.imageService = imageService
+        self.dataService = dataService
+    }
+}
